f271a58862c0e176e10eeab80684917f1d0cfc14
MentalSep <chahidjava@hotmail.com>
2024-04-25 19:32:52 +0100

    add task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- List Glam rock bands ranked by longevity
+SELECT band_name, IFNULL(split, 2022) - formed AS lifespan
+FROM metal_bands
+WHERE FIND_IN_SET("Glam rock", style)
+ORDER BY lifespan DESC;
