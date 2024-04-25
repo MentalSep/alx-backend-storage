2f9e2919be87863b0b84231a2e5692b3dbd867b0
MentalSep <chahidjava@hotmail.com>
2024-04-25 18:59:25 +0100

    add task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Create users table if it doesn't exist with unique emails and a country column
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
