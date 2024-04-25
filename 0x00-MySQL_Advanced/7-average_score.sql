65e6c511dfb56c4f621f25fb016a44870812cb5b
MentalSep <chahidjava@hotmail.com>
2024-04-25 20:17:56 +0100

    add task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,24 +1,25 @@
 -- Create stored procedure ComputeAverageScoreForUser
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+-- DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (
+  IN user_id INT
+)
 BEGIN
   DECLARE total_score FLOAT;
---   DECLARE num_corrections INT;
+  DECLARE num_corrections INT;
 
---   -- Calculate total score and number of corrections for the user
---   SELECT SUM(score), COUNT(*)
---   INTO total_score, num_corrections
---   FROM corrections
---   WHERE corrections.user_id = user_id;
+  -- Calculate total score and number of corrections for the user
+  SELECT SUM(score), COUNT(*)
+  INTO total_score, num_corrections
+  FROM corrections
+  WHERE corrections.user_id = user_id;
 
---   -- Update user's average score
---   IF num_corrections > 0 THEN
---     SET total_score = total_score / num_corrections;
---   ELSE
---     SET total_score = 0;  -- Set NULL for no corrections
---   END IF;
-  SET total_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+  -- Update user's average score
+  IF num_corrections > 0 THEN
+    SET total_score = total_score / num_corrections;
+  ELSE
+    SET total_score = 0;
+  END IF;
 
   UPDATE users
   SET average_score = total_score
