8afa7584b42bddcbc7bbe54a0200c9fe488287f5
MentalSep <chahidjava@hotmail.com>
2024-04-25 20:07:29 +0100

    add task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,27 +1,29 @@
 -- Create stored procedure ComputeAverageScoreForUser
+-- DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
+CREATE PROCEDURE ComputeAverageScoreForUser (
+  IN user_id INT
 )
 BEGIN
-    DECLARE total_score INT;
-    DECLARE total_projects INT;
+  DECLARE total_score INT;
+  DECLARE num_corrections INT;
 
-    -- Calculate total score for the user
-    SELECT SUM(score) INTO total_score
-    FROM corrections
-    WHERE user_id = user_id;
+  -- Calculate total score and number of corrections for the user
+  SELECT SUM(score), COUNT(*)
+  INTO total_score, num_corrections
+  FROM corrections
+  WHERE corrections.user_id = user_id;
 
-    -- Calculate total number of projects for the user
-    SELECT COUNT(DISTINCT project_id) INTO total_projects
-    FROM corrections
-    WHERE user_id = user_id;
+  -- Update user's average score
+  IF num_corrections > 0 THEN
+    SET total_score = total_score / num_corrections;
+  ELSE
+    SET total_score = NULL;  -- Set NULL for no corrections
+  END IF;
 
-    -- Compute average score for the user
-    IF total_projects > 0 THEN
-        UPDATE users
-        SET average_score = total_score / total_projects
-        WHERE id = user_id;
-    END IF;
-END //
+  UPDATE users
+  SET average_score = total_score
+  WHERE id = user_id;
+END;
+//
 DELIMITER ;
