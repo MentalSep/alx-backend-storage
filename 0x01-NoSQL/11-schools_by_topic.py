36425ea66c378ef44638f5d24ed9decb29ff2579
MentalSep <chahidjava@hotmail.com>
2024-04-30 21:07:53 +0100

    add shebang

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+"""Module schools_by_topic"""
+
+
 def schools_by_topic(mongo_collection, topic):
     """Return the list of schools having a specific topic"""
     return list(mongo_collection.find({"topics": topic}))
