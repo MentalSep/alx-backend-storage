c8485e60b6cb852588d534a172db43cdb629e0b3
MentalSep <chahidjava@hotmail.com>
2024-04-30 22:46:31 +0100

    add task 14

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+def top_students(mongo_collection):
+    """Returns all students sorted by average score in descending order"""
+
+    pipeline = [
+        {"$unwind": "$topics"},
+        {"$group": {
+                "_id": "$_id",
+                "name": {"$first": "$name"},
+                "topics": {"$push": "$topics"},
+                "averageScore": {"$avg": "$topics.score"}
+            }},
+        {"$sort": {"averageScore": -1}}
+        ]
+
+    cursor = mongo_collection.aggregate(pipeline)
+    return list(cursor)
