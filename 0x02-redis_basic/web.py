2f4e7778a61f7c9ed6f299ed3a3e9b9c654891df
MentalSep <chahidjava@hotmail.com>
2024-05-01 19:19:36 +0100

    test task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 """Module to store data in Redis."""
-import requests
-import redis
 from functools import wraps
 from typing import Callable
+import requests
+import redis
 
 
 def cache_and_track_access(fn: Callable) -> Callable:
-    """Decorator to cache the result of the request and track access"""
+    """Decorator for get_page"""
     @wraps(fn)
     def wrapper(url: str) -> str:
-        """Wrapper function"""
+        """ Wrapper function"""
         r = redis.Redis()
-        r.incr("count:{}".format(url))
-        cached_page = r.get("{}".format(url))
+        r.incr(f"count:{url}")
+        cached_page = r.get(f"{url}")
         if cached_page:
             return cached_page.decode("utf-8")
         response = fn(url)
-        r.set("{}".format(url), response, 10)
+        r.set(f"{url}", response, 10)
         return response
 
     return wrapper
@@ -25,5 +25,5 @@ def cache_and_track_access(fn: Callable) -> Callable:
 
 @cache_and_track_access
 def get_page(url: str) -> str:
-    """Fetch the page content"""
+    """ Get the content of a web page."""
     return requests.get(url).text
